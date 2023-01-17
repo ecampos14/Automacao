2047e5a93158545e6d63b2a3d35f77bd04bbad72
Emanuele  Campos <88864112+ecampos14@users.noreply.github.com>
2023-01-16 21:32:56 -0300

    Criação

---
+++ Email/email.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+#importar a base de dados
+
+tabelasVendas = pd.read_excel('Vendas.xlsx')
+
+
+#visualizar a base de dados
+
+# faturamento por loja 
+
+# quantidade de protudos
+
+# tickt medio por loja
+
+# enviar um email com relatorio
