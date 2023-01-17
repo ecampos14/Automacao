6b0f55e1e3c13ef463ab5c33c5c614117eb131a1
Emanuele  Campos <88864112+ecampos14@users.noreply.github.com>
2023-01-16 22:05:51 -0300

    Atualizaçao

--- Email/email.py
+++ Email/email.py
@@ -4,13 +4,17 @@ import pandas as pd
 
 tabelasVendas = pd.read_excel('Vendas.xlsx')
 
-
 #visualizar a base de dados
+pd.set_option('display.max_columns', None)
+print(tabelasVendas)
 
 # faturamento por loja 
+faturamento = tabelasVendas[['ID Loja','Valor Final']].groupby('ID Loja').sum()
+print(faturamento)
 
 # quantidade de protudos
+quantidade = tabelasVendas[['Id Loja', 'Quantidade']].groupby('ID Loja').sum()
 
 # tickt medio por loja
-
+ticket = (faturamento['Valor Final'] / quantidade['Quantidade']).to_frame()
 # enviar um email com relatorio
